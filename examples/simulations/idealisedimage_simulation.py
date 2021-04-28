1b5d963b51504b352ffcb60420a0029b33478283
Stephen Wilkins <stephenwilkins@Stephens-MacBook-Pro.local>
2021-04-28 11:23:22 +0100

    updated

--- examples/simulations/idealisedimage_simulation.py
+++ examples/simulations/idealisedimage_simulation.py
@@ -10,12 +10,16 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 import eritlux.simulations.simulations
 
+
+
+
+
 np.random.seed(42)
 
 args = sys.argv
 print(args)
 
-model = 'beta_idealised_EAZY'
+model = 'beta_idealisedimage_EAZY'
 
 if len(args) == 1:
     N = 10
@@ -42,15 +46,16 @@ elif len(args) == 4:
 else:
     print('ERROR: too many arguments')
 
-
-# --- define survey and field. This in turn defines the depth, area, and filters but this could be done manually.
-
 survey_id, field_id = 'SimpleHubble', '10nJy'
 field = FLARE.surveys.surveys[survey_id].fields[field_id]
 
-sim = eritlux.simulations.simulations.Simulation(profile_model = 'simple', sed_model = 'beta', output_filename = output_filename)
+detection_filters = [f'Hubble.WFC3.{f}' for f in ['f105w', 'f125w', 'f160w']]
+
+
+
+sim = eritlux.simulations.simulations.Simulation(profile_model = 'cSersic', sed_model = 'beta', output_filename = output_filename)
 sim.n(N)
 sim.intrinsic_beta(field.filters) # produce intrinsic photometry
-sim.photo_idealised(field) # produce observed photometry
+sim.photo_idealised_image(field, detection_filters = detection_filters) # produce observed photometry
 sim.pz_eazy() # measure photometric redshifts
 sim.export_to_HDF5(output_dir, output_filename)
